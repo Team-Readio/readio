745d148ab39a36349c522f9a06ab4a7fbcb00b20
BBB924 <qhrud4231@naver.com>
2025-05-09 02:56:41 +0900

    #2 feat(login): 로그인 프론트 화면 80% 구현, 폰트 적용

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from './layouts/Layout';
+import Login from "./pages/user/Login";
+import UserMain from "./pages/user/UserMain";
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+          <Route index element={<UserMain/>}/>
+          </Route>
+
+
+        <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
