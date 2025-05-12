5cf514d715ad7723710c9156d2e3556b085ad581
BBB924 <qhrud4231@naver.com>
2025-05-12 12:04:54 +0900

    #22 feat(account-suspended): 계정정지 안내 페이지 UI 구현 완료, 헤더 Link 반영/스타일 수정, 헤더 로고 정중앙 설정

--- Readio/src/App.jsx
+++ Readio/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from './layouts/Layout';
-import UserMain from "./pages/user/UserMain";
 import PostWriting from "./pages/post/PostWriting";
+import AccountSuspended from "./pages/user/AccountSuspended";
+import UserMain from "./pages/user/UserMain";
 
 function App() {
 
@@ -9,10 +10,12 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout/>}>
-          <Route index element={<UserMain/>}/>
-          <Route path="post/writing" element={<PostWriting/>}/>
-        </Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<UserMain />} />
+
+            <Route path="post/writing" element={<PostWriting />} />
+            <Route path="account/suspended" element={<AccountSuspended/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
