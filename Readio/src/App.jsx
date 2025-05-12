99c01b061372170b2be6980d1bf6156d94236729
BBB924 <qhrud4231@naver.com>
2025-05-12 10:37:42 +0900

    #20 아이디, 비밀번호 찾기 페이지 추가

--- Readio/src/App.jsx
+++ Readio/src/App.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from './layouts/Layout';
 import PostWriting from "./pages/post/PostWriting";
 import FindAccount, { FindIdForm, FindPwdForm } from "./pages/user/FindAccount";
 import Join from "./pages/user/Join";
+import JoinComplete from "./pages/user/JoinComplete";
 import Login from "./pages/user/Login";
 import NoticeList from './pages/user/NoticeList';
 import UserMain from "./pages/user/UserMain";
-import JoinComplete from "./pages/user/JoinComplete";
 
 function App() {
 
@@ -24,16 +23,15 @@ function App() {
             <Route path="/login" element={<Login />} /> {/* 로그인 */}
             <Route path="/join" element={<Join />} /> {/* 회원가입 */}
             <Route path="/join/complete" element={<JoinComplete />} /> {/* 회원가입 완료 */}
-          </Route>
 
-        <Route path="/find-account" element={<FindAccount />}>
-          <Route index element={<Navigate to="find-id" replace />} />
-          <Route path="find-id" element={<FindIdForm />} />
-          <Route path="find-pwd" element={<FindPwdForm />} />
+          <Route path="/find-account" element={<FindAccount />}>  
+            <Route index element={<Navigate to="find-id" replace />} />   {/* 기본 페이지-아이디찾기 */}
+            <Route path="find-id" element={<FindIdForm />} />             {/* 아이디 찾기 */}
+            <Route path="find-pwd" element={<FindPwdForm />} />           {/* 비밀번호 찾기 */}
           </Route>
         </Route>
-        </Routes>
-      </BrowserRouter>
+      </Routes>
+    </BrowserRouter >
     </>
   );
 }
