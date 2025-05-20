2fd71192b74c4c336d04092db25f4d8d3bfa8195
BBB924 <qhrud4231@naver.com>
2025-05-20 11:35:22 +0900

    - 회원가입용 브랜치로 재 적용
    - 2025.05.20 11:25 기준 공통 컴포넌트 적용
    - 로그인, 회원가입 외 모든 컴포넌트 삭제

--- Readio/src/main.jsx
+++ Readio/src/main.jsx
@@ -1,11 +1,6 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'; // createRoot를 직접 import
-import App from './App.jsx';
-import { Provider } from 'react-redux';
-import { store } from './store';
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-);
+    <App />
+)
