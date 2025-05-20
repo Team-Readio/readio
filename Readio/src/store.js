a86c7b0047b3b529b885eec0ca817d19f922b4c6
BBB924 <qhrud4231@naver.com>
2025-05-20 11:36:41 +0900

    회원가입용 브랜치로 재 적용 #4
    - 2025.05.20 11:25 기준 공통 컴포넌트 적용
    - 로그인, 회원가입 외 모든 컴포넌트 삭제

--- Readio/src/store.js
+++ Readio/src/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import bookSearchReducer from './components/modules/postwriting/bookSearchSlice';
+// import { configureStore } from "@reduxjs/toolkit";
+// import rootReducers from './modules/index';
 
-export const store = configureStore({
-    reducer: {
-        bookSearch: bookSearchReducer,
-    },
-});
+// const store = configureStore ({
+//     reducer: rootReducers,
+//     // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+//     devTools: true, // 개발환경이면 true, 배포 시 false로 바꿔도 됨
+// }
+// );
+// export default store;
