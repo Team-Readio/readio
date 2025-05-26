14b9182a363dfa169a549e86ded519614618f0f4
BBB924 <qhrud4231@naver.com>
2025-05-26 15:38:16 +0900

    Feat: 헤더 로그아웃 기능 추가 #92

--- Readio/src/modules/index.js
+++ Readio/src/modules/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import FilteringSlice from "./filtering/FilteringSlice.js";
+import userReducer from './logout/userReducer';
 import bookSearchSlice from "./postwriting/bookSearchSlice.js";
 import CurationSlice from "./video/CurationSlice.js";
 import videoSlice from "./video/VideoSlice.js";
@@ -10,7 +11,8 @@ const rootReducers =  combineReducers(
             filtering: FilteringSlice,
             curation: CurationSlice,
             bookSearch: bookSearchSlice,
-            video: videoSlice
+            video: videoSlice,
+            user: userReducer
         });
 
 export default rootReducers;
